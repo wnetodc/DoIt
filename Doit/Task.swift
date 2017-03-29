e8793b3a9e6cf3886aaa224a3b9eebb6550cc876
wellington neto <wellington.neto@gmail.com>
2017-03-29 20:34:40 -0300

    criação da class Task
    criação da classe Task.swif

---
+++ Doit/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Doit
+//
+//  Created by wellington neto on 29/03/17.
+//  Copyright © 2017 wneto. All rights reserved.
+//
+
+import Foundation
+
+class Task{
+    var name = ""
+    var important = false
+}
